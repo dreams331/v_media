5bda1ef0a70f5b4668fba4df117f58f9780455ac
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-09-11 10:18:40 +0100

    Update bigquery.tf

--- bigquery.tf
+++ bigquery.tf
@@ -2,11 +2,10 @@
 resource "google_bigquery_dataset_access" "access" {
   dataset_id    = google_bigquery_dataset.dataset.dataset_id
   role          = "roles/bigquery.dataEditor"
-  user_by_email = google_service_account.bqowner.email
+  user_by_email = google_service_account.v_media_service_account.email
 }
 
 resource "google_bigquery_dataset" "dataset" {
   dataset_id = "v_media_dataset"
 }
-    
     
