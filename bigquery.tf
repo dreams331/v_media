57e68a4c65076b734387d32d0b89a212e5b6ee55
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-09-11 10:13:27 +0100

    Update bigquery.tf

--- bigquery.tf
+++ bigquery.tf
@@ -1,46 +1,12 @@
-locals {
-  tables             = { for table in var.tables : table["table_id"] => table }
-  views              = { for view in var.views : view["view_id"] => view }
-  materialized_views = { for mat_view in var.materialized_views : mat_view["view_id"] => mat_view }
-  external_tables    = { for external_table in var.external_tables : external_table["table_id"] => external_table }
-  routines           = { for routine in var.routines : routine["routine_id"] => routine }
-
-  iam_to_primitive = {
-    "roles/bigquery.dataOwner" : "OWNER"
-    "roles/bigquery.dataEditor" : "WRITER"
-    "roles/bigquery.dataViewer" : "READER"
-  }
+  
+resource "google_bigquery_dataset_access" "access" {
+  dataset_id    = google_bigquery_dataset.dataset.dataset_id
+  role          = "roles/bigquery.dataEditor"
+  user_by_email = google_service_account.bqowner.email
 }
 
-resource "google_bigquery_dataset" "main" {
-  dataset_id                  = var.dataset_id
-  friendly_name               = var.dataset_name
-  description                 = var.description
-  location                    = var.location
-  delete_contents_on_destroy  = var.delete_contents_on_destroy
-  default_table_expiration_ms = var.default_table_expiration_ms
-  project                     = var.project_id
-  labels                      = var.dataset_labels
-
-  dynamic "default_encryption_configuration" {
-    for_each = var.encryption_key == null ? [] : [var.encryption_key]
-    content {
-      kms_key_name = var.encryption_key
-    }
-  }
-
-  dynamic "access" {
-    for_each = var.access
-    content {
-      # BigQuery API converts IAM to primitive roles in its backend.
-      # This causes Terraform to show a diff on every plan that uses IAM equivalent roles.
-      # Thus, do the conversion between IAM to primitive role here to prevent the diff.
-      role = lookup(local.iam_to_primitive, access.value.role, access.value.role)
-
-      domain         = lookup(access.value, "domain", null)
-      group_by_email = lookup(access.value, "group_by_email", null)
-      user_by_email  = lookup(access.value, "user_by_email", null)
-      special_group  = lookup(access.value, "special_group", null)
-    }
-  }
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "v_media_dataset"
 }
+    
+    
